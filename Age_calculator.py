c76b62a03f05f806c15dec506d1be281a8ac476c
yosh22-raj <yoshraj145@gmail.com>
2025-06-25 13:52:20 +0200

    commit (file and list to store records of age)

--- Age_calculator.py
+++ Age_calculator.py
@@ -2,3 +2,9 @@ import tkinter as tk
 from tkinter import messagebox
 from Age_calculator import DOB, calculate_AGE, write_record_to_file
 
+# File to store age records
+DATA_FILE = "age_records.txt"
+
+# List to keep session age records
+age_records = []
+
