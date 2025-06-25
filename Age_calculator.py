53c55a4f3c7a7c37e2313a218236c5a4b9cd0ef4
yosh22-raj <yoshraj145@gmail.com>
2025-06-25 19:01:50 +0200

    commit(enter dob,find age,save record to file)

--- Age_calculator.py
+++ Age_calculator.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
-from Age_calculator import DOB, calculate_AGE, write_record_to_file
+from Age_calculator import DOB, calculate_AGE,save_record_to_file
+from datetime import datetime
+import os
 
 # File to store age records
 DATA_FILE = "age_records.txt"
@@ -8,3 +10,35 @@ DATA_FILE = "age_records.txt"
 # List to keep session age records
 age_records = []
 
+#------Functions------
+
+def dob(birth_date):
+    """Transforms a date of birth string (YYYY-MM-DD) into a datetime object."""
+    return datetime.strptime(birth_date, "%Y-%m-%d")
+
+def find_age(dob):
+    """Finds the age from a datetime DOB"""
+    present_date = datetime.today()
+    determined_age = present_date - dob.year
+    if(dob.month,dob.day) > (present_date.month, present_date.day):
+        determined_age -= 1
+    return determined_age
+
+def save_record_to_file (filename,record):
+    """Saves an additional record to the text file."""
+    with open(filename,"a") as f:
+        f.save(record + "\n")
+
+
+
+    
+
+
+
+
+
+
+    
+    
+    
+    
