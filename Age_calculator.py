c8c34f9334b408380612ff09670c2cce72fdea36
yosh22-raj <yoshraj145@gmail.com>
2025-06-26 13:32:35 +0200

    commit(finishes till gui)

--- Age_calculator.py
+++ Age_calculator.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from Age_calculator import DOB, calculate_AGE,save_details_to_file
 from datetime import datetime
-import os
 
 # File to store age details
 DATA_FILE = "age_details.txt"
@@ -17,9 +15,9 @@ def dob(birth_date):
     return datetime.strptime(birth_date, "%Y-%m-%d")
 
 def find_age(dob):
-    """Finds the age from a datetime DOB"""
+    """Finds the age from a datetime dob"""
     present_date = datetime.today()
-    determined_age = present_date - dob.year
+    determined_age = present_date.year - dob.year
     if(dob.month,dob.day) > (present_date.month, present_date.day):
         determined_age -= 1
     return determined_age
@@ -27,22 +25,18 @@ def find_age(dob):
 def save_details_to_file (filename,details):
     """Saves an additional details to the text file."""
     with open(filename,"a") as f:
-        f.save(details + "\n")
+        f.write(details + "\n")
 
+def find_age_gui():
+    """runs when the user click the button to find age"""
+    dob_input = dob_entry.get()
+    try:
+        dob_date = dob(dob_input)
+        age = find_age(dob_date)
+        result_label.config(text=f"Age: {age} years")
+        details = f"DOB: {dob_input} => Age: {age} years"
+        save_details_to_file(DATA_FILE, details)
+        messagebox.showinfo("Success", "Age Evaluated and saved successfully.")
+    except ValueError:
+        messagebox.showerror("Weak Input", "Please enter the correct DOB in YYYY-MM-DD format.")
 
-        
-
-
-
-
-    
-
-
-
-
-
-
-    
-    
-    
-    
